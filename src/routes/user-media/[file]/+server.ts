b4ce5c6e567c86c379781a88bcd06298c36054b2
doodlezucc <electerminator@gmail.com>
2025-05-10 13:53:10 +0200

    Improve user-media requests with Last-Modified header

--- src/routes/user-media/[file]/+server.ts
+++ src/routes/user-media/[file]/+server.ts
@@ -4,18 +4,29 @@ import { createReadableStream } from '@sveltejs/kit/node';
 import * as fs from 'fs/promises';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params, request }) => {
 	const userMediaPath = server.assetManager.resolveAssetPath(params.file, {
 		accessibleByBackend: true
 	});
 
+	const fileStats = await fs.stat(userMediaPath);
+	const contentLength = fileStats.size;
+	const lastModified = fileStats.ctime;
+	lastModified.setMilliseconds(0);
+
+	const ifModifiedSinceHeader = request.headers.get('If-Modified-Since');
+	if (ifModifiedSinceHeader) {
+		if (lastModified.getTime() <= Date.parse(ifModifiedSinceHeader)) {
+			return new Response(null, { status: 304 });
+		}
+	}
+
 	const fileExtension = userMediaPath.substring(userMediaPath.lastIndexOf('.') + 1);
 	const mimeType = findMimeTypeForExtension(fileExtension);
 
-	const contentLength = (await fs.stat(userMediaPath)).size;
-
 	return new Response(createReadableStream(userMediaPath), {
 		headers: {
+			'Last-Modified': lastModified.toUTCString(),
 			'Content-Length': `${contentLength}`,
 			...(mimeType ? { 'Content-Type': mimeType } : {})
 		}
